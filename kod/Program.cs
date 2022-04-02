8f9660cbe42239491e08d7886dba9c053c11010d
zamira <zamira-c@bk.ru>
2022-04-02 23:07:03 +0300

    Код программы

---
+++ kod/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+string [] arr = new string[] { "1", "sdfsd", "12", "2123авваив", "йц654", "0о"};
+int f = arr.Length;
+string [] tempArr = new string [f];
+int j = 0;
+ 
+foreach (string i in arr)       //проход по каждому элементу массива
+{
+    int length = i.Length;
+    if (length <= 3) 
+   { 
+    tempArr [j] =i;
+    j++;
+    }
+}
+string [] arrFin = new string [j];
+j=0;
+foreach (string i in tempArr) 
+{
+    if (i != null)
+    {
+     arrFin[j] = i;
+    WriteLine (arrFin[j]);
+    j++;
+    }
+}
